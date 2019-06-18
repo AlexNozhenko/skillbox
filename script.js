b43cbcd1198d921186d517c59974ef1211e5d93a
User A0309972 <a0309972@violektrina.from.sh>
2019-06-18 09:09:38 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+$(function(){
+  $(".button_call_top, .button_icon_phone, .button_order, .button_call_footer").click(function(){
+  $(".form-container").css("display", "flex");
+  });
+
+  $(".button_icon_menu").click(function(){
+  $(".popup-menu").css("display", "block");
+  $(".button_icon_menu").hide();
+  $(".button_icon_close").show();
+  });
+
+  $(".button_icon_close").click(function(){
+    $(".popup-menu, .button_icon_close").hide();
+    $(".button_icon_menu").show();
+  });
+
+  $(document).mouseup(function (e){ 
+    var div = $(".form"); 
+    if (!div.is(e.target) 
+        && div.has(e.target).length === 0) {
+      $(".form-container").hide();
+    }
+  });
+
+  $(document).ready(function(){
+        $(".owl-carousel").owlCarousel({
+            loop:true,
+            margin:10,
+            nav:true,
+            responsive:{
+                0:{
+                    items:1
+                },
+                700:{
+                    items:2
+                },
+                1100:{
+                    items:3
+                }
+            }
+        });
+    });
+});
